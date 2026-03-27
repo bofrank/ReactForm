db5c50c285bcb6eea2176cf7c47f85df284ef4bd
bofrank <bo@bofrank.com>
2026-03-27 22:12:28 +0900

    Componetizing validation.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
 
   function handleSubmit(event){
     event.preventDefault();
@@ -7,6 +11,12 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll('acquisition');
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if(data.password !== data['confirm-password']){
+      setPasswordsAreNotEqual(true);
+      return;
+    }
+
     console.log(data);
   }
 
@@ -17,13 +27,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" required minLength={6} />
         </div>
 
         <div className="control">
@@ -33,6 +43,7 @@ export default function Signup() {
             type="password"
             name="confirm-password"
           />
+          <div className="control-error">{passwordsAreNotEqual && <p>Passwords must match.</p>}</div>
         </div>
       </div>
 
@@ -91,7 +102,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" required />I
           agree to the terms and conditions
         </label>
       </div>
